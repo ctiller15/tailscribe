1cafba416c20770693f665d2a6a34119543f18b4
ctiller15 <christiller2015@gmail.com>
2025-10-29 20:58:16 -0400

    cleanup

--- scripts/init_test.sh
+++ scripts/init_test.sh
@@ -7,6 +7,6 @@ fi
 
 # Name and port defined in docker-compose
 dbconnstr="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
-``
+
 cd sql/schema 
 goose postgres $dbconnstr up
